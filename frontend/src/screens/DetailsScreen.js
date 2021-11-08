15567bf8c19e66067ab1bfdff2867f3ea36996f4
Sambasivarao <samba.pendela@gmail.com>
2021-11-08 19:46:34 +0530

    code

---
+++ frontend/src/screens/DetailsScreen.js
@@ -0,0 +1,12 @@
+//receive the id from HomeScreen
+//send the id to the DetailsAction
+
+import React from "react";
+
+function DetailsScreen(){
+    return(
+        <React.Fragment></React.Fragment>
+    )
+}
+
+export default DetailsScreen;
