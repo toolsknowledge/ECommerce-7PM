0879d899ec17537c738cd8bf97677fa4b69ff359
Sambasivarao <samba.pendela@gmail.com>
2021-12-24 19:43:34 +0530

    forms

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -12,6 +12,20 @@ function RegisterScreen(){
     const validate = values=>{
         const errors = {};
 
+        if(!values.fname){
+            errors.fname = "First Name Required";    
+        }
+
+        if(!values.lname){
+            errors.lname = "Last Name Required";
+        }
+
+        if(!values.email){
+            errors.email = "Email is Required";
+        }else if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)){
+            errors.email = "Invalid Email Format";
+        }
+
         return errors;
     }
 
@@ -21,23 +35,50 @@ function RegisterScreen(){
         validate
     });
 
-
+    console.log( formik.touched );
 
 
     return(
         <React.Fragment>
             <div className="MainForm">
                 <form onSubmit={formik.handleSubmit}>
-                    <label htmlFor="fname">First Name</label>
-                    <input type="text" name="fname" id="fname" onChange={formik.handleChange} value={formik.values.fname}></input>
 
-                    <label htmlFor="lname">Last Name</label>
-                    <input type="text" name="lname" id="lname" onChange={formik.handleChange} value={formik.values.lname}></input>
+                    <div className="form-control">
+                        <label htmlFor="fname">First Name</label>
+                        <input type="text" 
+                               name="fname" 
+                               id="fname" 
+                               onChange={formik.handleChange} 
+                               value={formik.values.fname}
+                               onBlur={formik.handleBlur}></input>
+                        {formik.touched.fname && formik.errors.fname?<div className="error">{formik.errors.fname}</div>:null}
+                    </div>
 
-                    <label htmlFor="Email">Email</label>
-                    <input type="email" name="email" id="email" onChange={formik.handleChange} value={formik.values.email}></input>
+                    <div className="form-control">
+                        <label htmlFor="lname">Last Name</label>
+                        <input type="text" 
+                               name="lname" 
+                               id="lname" 
+                               onChange={formik.handleChange} 
+                               value={formik.values.lname}
+                               onBlur={formik.handleBlur}></input>
+                        {formik.touched.lname && formik.errors.lname?<div className="error">{formik.errors.lname}</div>:null}
+                    </div>
 
-                    <input type="submit"></input>
+                    <div className="form-control">
+                        <label htmlFor="Email">Email</label>
+                        <input type="email" 
+                               name="email" 
+                               id="email" 
+                               onChange={formik.handleChange} 
+                               value={formik.values.email}
+                               onBlur={formik.handleBlur}></input>
+                        {formik.touched.email && formik.errors.email?<div className="error">{formik.errors.email}</div>:null}
+                    </div>
+                    
+                    <div className="form-control">
+                        <input type="submit"></input>
+                    </div>
                 </form>
             </div>
         </React.Fragment>
