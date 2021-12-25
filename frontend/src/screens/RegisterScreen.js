85bb57d510674461f3060deeb914b62db3571d12
Sambasivarao <samba.pendela@gmail.com>
2021-12-25 18:06:04 +0530

    forms

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -1,85 +1,64 @@
 import React from "react";
-import { useFormik } from "formik";
+import { ErrorMessage,Formik,Field, Form } from "formik";
+import * as Yup from "yup";
 function RegisterScreen(){
-
     const initialValues = {fname:"",lname:"",email:""};
-
-
     const onSubmit = values=>{
         console.log(values);
     }
-
-    const validate = values=>{
-        const errors = {};
-
-        if(!values.fname){
-            errors.fname = "First Name Required";    
-        }
-
-        if(!values.lname){
-            errors.lname = "Last Name Required";
-        }
-
-        if(!values.email){
-            errors.email = "Email is Required";
-        }else if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)){
-            errors.email = "Invalid Email Format";
-        }
-
-        return errors;
-    }
-
-    const formik = useFormik({
-        initialValues,
-        onSubmit,
-        validate
-    });
-
-    console.log( formik.touched );
-
-
+    const validateSchema = Yup.object({
+        fname:Yup.string().required("fname required !"),
+        lname:Yup.string().required("lname required !"),
+        email : Yup.string().email("invalid email format").required("email is required"),
+        address:Yup.string().max(25,"should be 25 characters").required("Address is Required")
+    })
     return(
         <React.Fragment>
             <div className="MainForm">
-                <form onSubmit={formik.handleSubmit}>
-
+                <Formik initialValues={initialValues}
+                        validationSchema={validateSchema}
+                        onSubmit={onSubmit}>
+                <Form>
                     <div className="form-control">
                         <label htmlFor="fname">First Name</label>
-                        <input type="text" 
+                        <Field type="text" 
                                name="fname" 
-                               id="fname" 
-                               onChange={formik.handleChange} 
-                               value={formik.values.fname}
-                               onBlur={formik.handleBlur}></input>
-                        {formik.touched.fname && formik.errors.fname?<div className="error">{formik.errors.fname}</div>:null}
+                               id="fname"></Field>
+                        <ErrorMessage name="fname"></ErrorMessage>
                     </div>
 
                     <div className="form-control">
                         <label htmlFor="lname">Last Name</label>
-                        <input type="text" 
+                        <Field type="text" 
                                name="lname" 
-                               id="lname" 
-                               onChange={formik.handleChange} 
-                               value={formik.values.lname}
-                               onBlur={formik.handleBlur}></input>
-                        {formik.touched.lname && formik.errors.lname?<div className="error">{formik.errors.lname}</div>:null}
+                               id="lname" ></Field>
+                        
+                        <ErrorMessage name="lname"></ErrorMessage> 
                     </div>
 
                     <div className="form-control">
                         <label htmlFor="Email">Email</label>
-                        <input type="email" 
+                        <Field type="email" 
                                name="email" 
-                               id="email" 
-                               onChange={formik.handleChange} 
-                               value={formik.values.email}
-                               onBlur={formik.handleBlur}></input>
-                        {formik.touched.email && formik.errors.email?<div className="error">{formik.errors.email}</div>:null}
+                               id="email"></Field>
+                        
+                        <ErrorMessage name="email"></ErrorMessage>
                     </div>
-                    
+
+                    <div className="form-control">
+                        <label htmlFor="Address">Address</label>
+                        <Field as="textarea"
+                               name="address"
+                               id="address"></Field>
+                        <ErrorMessage name="address"></ErrorMessage>
+                    </div>
+
+
                     <div className="form-control">
                         <input type="submit"></input>
                     </div>
-                </form>
+                </Form>
+                </Formik>
             </div>
         </React.Fragment>
     )
